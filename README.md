81b5c9126b2d388cc0d6b64b15d599703502f7b8
Nevin Leh <nevinleh@proton.me>
2024-05-08 11:08:43 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Lexer for the monkey language
+To build
+mkdir build
+cd build
+cmake ..
+cmake --build .
---
+++ src/token.hpp
@@ -0,0 +1,6 @@
+#include <string.h>
+typdef std::string TokenType;
+struct Token {
+    TokenType Type;
+    std::string sliteral;
+};
